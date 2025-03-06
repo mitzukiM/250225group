6a51823ec2b9a848da7e48606e7da07ed1b4f8e1
MitzukiM <johndoe@example.com>
2025-03-06 18:58:09 +0200

    homework

--- homework_5.py
+++ homework_5.py
@@ -3,5 +3,5 @@ smile_emoji = "\U0000263a"
 
 user_name = "Rita"
 user_age = "14"
-result = f"My name is  {user_name}  I am {user_age} years old {smile_emoji}"
+result = f" My name is  {user_name}  I am {user_age} years old {smile_emoji}"
 print(result)
